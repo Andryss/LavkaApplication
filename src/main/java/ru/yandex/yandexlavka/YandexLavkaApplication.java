851e169514dbf011bcf58b5285319a9ae7d0f4c5
Andryss <andr.krivosheev@gmail.com>
2023-04-04 22:21:29 +0300

    Delete unused imports

--- src/main/java/ru/yandex/yandexlavka/YandexLavkaApplication.java
+++ src/main/java/ru/yandex/yandexlavka/YandexLavkaApplication.java
@@ -1,12 +1,7 @@
 package ru.yandex.yandexlavka;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @SpringBootApplication
 public class YandexLavkaApplication {

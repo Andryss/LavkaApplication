93b4ddb0e763a35ca51ebdba3dc3153c9bef1204
Andryss <andr.krivosheev@gmail.com>
2023-04-12 15:41:53 +0300

    Extend repo for GetCourierMetaInfo handle

--- src/main/java/ru/yandex/yandexlavka/repositories/OrderRepository.java
+++ src/main/java/ru/yandex/yandexlavka/repositories/OrderRepository.java
@@ -2,11 +2,14 @@ package ru.yandex.yandexlavka.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.yandex.yandexlavka.entities.couriers.CourierEntity;
 import ru.yandex.yandexlavka.entities.orders.OrderEntity;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
     List<OrderEntity> findAllByOrderIdIn(List<Long> idList);
+    List<OrderEntity> findAllByAssignedCourierIdAndCompletedTimeGreaterThanEqualAndCompletedTimeLessThan(CourierEntity courierId, LocalDate startDate, LocalDate endDate);
 }

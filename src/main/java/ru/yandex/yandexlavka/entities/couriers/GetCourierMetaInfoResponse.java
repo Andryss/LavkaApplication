ae1144a90bd31dcfdaf85309c498be95014b5fcc
Andryss <andr.krivosheev@gmail.com>
2023-04-12 15:43:09 +0300

    "Lomboked" class

--- src/main/java/ru/yandex/yandexlavka/entities/couriers/GetCourierMetaInfoResponse.java
+++ src/main/java/ru/yandex/yandexlavka/entities/couriers/GetCourierMetaInfoResponse.java
@@ -1,7 +1,14 @@
 package ru.yandex.yandexlavka.entities.couriers;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetCourierMetaInfoResponse {
     Long courierId;
     CourierType courierType;

57efc3b71a0ba4dd4e523074d3085ca75e172ede
Andryss <andr.krivosheev@gmail.com>
2023-07-21 14:44:52 +0300

    Fix CompleteOrderRequestDto validation

--- src/main/java/ru/yandex/yandexlavka/objects/mapping/complete/order/CompleteOrderRequestDto.java
+++ src/main/java/ru/yandex/yandexlavka/objects/mapping/complete/order/CompleteOrderRequestDto.java
@@ -1,6 +1,7 @@
 package ru.yandex.yandexlavka.objects.mapping.complete.order;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,7 +12,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CompleteOrderRequestDto {
-    @NotNull
-    List<@Valid CompleteOrder> completeInfo;
+public class CompleteOrderRequestDto { // TODO: CompleteOrderRequest
+    @NotEmpty
+    List<@NotNull @Valid CompleteOrder> completeInfo;
 }
